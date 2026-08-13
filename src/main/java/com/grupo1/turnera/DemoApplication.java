36396c09ca9437819aa53a91f4dc2ab8912df2a8
Gian <gbisbini@uade.edu.ar>
2026-08-12 23:36:11 -0300

    Initial commit

---
+++ src/main/java/com/grupo1/turnera/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.grupo1.turnera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo1/turnera/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.turnera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
