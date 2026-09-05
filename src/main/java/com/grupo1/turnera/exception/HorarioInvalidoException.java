11cbf98505aa76d412a50aa87088ae0d784b01b7
eugeA21 <m.euge.alonso1@gmail.com>
2026-09-05 19:05:23 -0300

    Add HorarioInvalidoException class for error handling

---
+++ src/main/java/com/grupo1/turnera/exception/HorarioInvalidoException.java
@@ -0,0 +1,8 @@
+package com.grupo1.turnera.exception;
+
+public class HorarioInvalidoException extends RuntimeException {
+
+    public HorarioInvalidoException(String mensaje) {
+        super(mensaje);
+    }
+} 
